6b7378d1291a713484ef53d3225617bd3d2e607d
Jonathan Pyers <pyersjonathan@gmail.com>
2023-04-04 16:08:54 -0600

    Refactor.

--- src/common/command.ts
+++ src/common/command.ts
@@ -1,43 +1,26 @@
 interface CommandData {
-	id: number;
-	command: number;
-	payload: any;
+  id: number;
+  command: number;
+  payload: any;
 }
 
 export class Command {
-  // Converts a CommandData object into a Buffer.
-	static toBuffer(command: CommandData): Buffer {
-		if (typeof command.payload === "undefined") {
-			throw new TypeError("The payload must not be undefined!");
-		}
+  static toBuffer({ payload, id, command }: CommandData): Buffer {
+    if (payload === undefined) throw new TypeError("The payload must not be undefined!");
+    const payloadString = JSON.stringify(payload);
+    const buffer = Buffer.allocUnsafe(payloadString.length + 3);
+    buffer.writeUInt16LE(id, 0);
+    buffer.writeUInt8(command, 2);
+    buffer.write(payloadString, 3);
+    return buffer;
+  }
 
-		const payloadString = JSON.stringify(command.payload);
-		const payload = Buffer.from(payloadString);
-		const buffer = Buffer.allocUnsafe(payload.length + 3);
-
-		buffer.writeUInt16LE(command.id, 0);
-		buffer.writeUInt8(command.command, 2);
-		payload.copy(buffer, 3);
-
-		return buffer;
-	}
-
-  // Converts a Buffer into a CommandData object.
-	static parse(buffer: Buffer): CommandData {
-		if (buffer.length < 3) {
-			throw new TypeError("Token too short! ${token}");
-		}
-
-		const id = buffer.readUInt16LE(0);
-		const command = buffer.readUInt8(2);
-		const payloadString = buffer.subarray(3).toString();
-		const payload = JSON.parse(payloadString);
-
-		return {
-			id,
-			command,
-			payload,
-		};
-	}
+  static parse(buffer: Buffer): CommandData {
+    if (buffer.length < 3) throw new TypeError(`Token too short! Expected at least 3 bytes, got ${buffer.length}!`);
+    const id = buffer.readUInt16LE(0);
+    const command = buffer.readUInt8(2);
+    const payloadString = buffer.toString("utf8", 3);
+    const payload = JSON.parse(payloadString);
+    return { id, command, payload };
+  }
 }
-
