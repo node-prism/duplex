a7c76945e56f9f610516044e3177e60ca3301273
nvms <jon@pye.rs>
2023-07-23 18:56:51 -0400

    Additional exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { CommandClient } from "./client/commandclient";
-export { CommandServer } from "./server/commandserver";
+export { CommandClient, TokenClientOptions } from "./client/commandclient";
+export { CommandServer, TokenServerOptions } from "./server/commandserver";
 export { Connection } from "./common/connection";
 export { CodeError } from "./common/codeerror";
 export { Status } from "./common/status";
